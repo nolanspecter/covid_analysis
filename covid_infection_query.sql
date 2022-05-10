89c02d2ab456727874427cfb2684ff5c733876a0
Duc Dang <anhduc031100@gmail.com>
2022-05-09 22:48:17 -0400

    Covid Infection Query

--- covid_infection_query.sql
+++ covid_infection_query.sql
@@ -52,5 +52,49 @@ WHERE continent IS NOT NULL
 GROUP BY location, population
 ORDER BY death_rate DESC;
 
+-- Continent figures
+-- Continent case count
+WITH continent_total_cases (continent, location, total_cases) AS
+	(SELECT continent, location, MAX(total_cases) AS total_cases
+	FROM Portfolio..infection_and_death
+	WHERE continent IS NOT NULL
+	GROUP BY continent, location)
+SELECT continent, SUM(total_cases) AS continent_case_count
+FROM continent_total_cases
+GROUP BY continent
+ORDER BY continent_case_count DESC;
 
+-- Continent death count
+WITH continent_total_death (continent, location, total_deaths) AS
+	(SELECT continent, location, MAX(CAST(total_deaths AS int)) AS total_deaths
+	FROM Portfolio..infection_and_death
+	WHERE continent IS NOT NULL
+	GROUP BY continent, location)
+SELECT continent, SUM(total_deaths) AS continent_death_count
+FROM continent_total_death
+GROUP BY continent
+ORDER BY continent_death_count DESC;
 
+-- Continent death rate
+WITH continent_total_death (continent, location, total_cases, total_deaths) AS
+	(SELECT continent, location, MAX(total_cases) AS total_cases, MAX(CAST(total_deaths AS int)) AS total_deaths
+	FROM Portfolio..infection_and_death
+	WHERE continent IS NOT NULL
+	GROUP BY continent, location)
+SELECT continent, 
+		SUM(total_cases) AS continent_total_cases, 
+		SUM(total_deaths) AS continent_total_deaths, 
+		SUM(total_deaths)/SUM(total_cases) * 100 AS continent_death_rate
+FROM continent_total_death
+GROUP BY continent
+ORDER BY continent_death_rate DESC;
+
+-- Global figures
+SELECT date, 
+		SUM(total_cases) AS global_case_count, 
+		SUM(CAST(total_deaths AS int)) AS global_death_count, 
+		SUM(CONVERT(int, total_deaths))/SUM(total_cases) * 100 AS global_death_rate
+FROM Portfolio..infection_and_death
+WHERE continent IS NOT NULL
+GROUP BY date
+ORDER BY date;
